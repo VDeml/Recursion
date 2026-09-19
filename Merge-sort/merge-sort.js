7e1521b4eec6b1153852c0a63c6cd394e2bc8b58
Vojtěch Deml <235454976+VDeml@users.noreply.github.com>
2026-09-19 17:25:52 +0200

    Fully solve merge-sort problem and add 2 tests

--- Merge-sort/merge-sort.js
+++ Merge-sort/merge-sort.js
@@ -1,12 +1,39 @@
-function mergeSort(array) {
-    function splitInHalf(array) {
-        const leftHalf = array.slice(0, (array.length / 2))
-        const rightHalf = array.slice((array.length / 2), array.length)
-        return { leftHalf, rightHalf }
+function mergeSort(array = []) {
+    if(array.length <=1) {
+        return array
     }
-    return "yes"
+    function algorithmMerge(sortedArrOne, sortedArrTwo) {
+        let i = 0;
+        let j = 0;
+        let sortedMergedArr = []
+        while(i < sortedArrOne.length && j < sortedArrTwo.length) {
+            if(sortedArrOne[i] <= sortedArrTwo[j]) {
+                sortedMergedArr.push(sortedArrOne[i])
+                i++
+            }
+            else {
+                sortedMergedArr.push(sortedArrTwo[j])
+                j++
+            }
+        }
+        for(; i < sortedArrOne.length; i++) {
+            sortedMergedArr.push(sortedArrOne[i])
+        }
+        for(; j < sortedArrTwo.length; j++) {
+            sortedMergedArr.push(sortedArrTwo[j])
+        }
+        return sortedMergedArr;
+    }
+
+    const leftHalf = array.slice(0, (array.length / 2))
+    const rightHalf = array.slice((array.length / 2), array.length)
+
+    const sortedLeftHalf = mergeSort(leftHalf)
+    const sortedRighHalf = mergeSort(rightHalf)
+
+    return algorithmMerge(sortedLeftHalf, sortedRighHalf)
 }
 
 
 
-export{ mergeSort }
+export { mergeSort }
--- Merge-sort/merge-sort.test.js
+++ Merge-sort/merge-sort.test.js
@@ -1,6 +1,13 @@
 import { test, expect } from "@jest/globals";
 import { mergeSort } from "./merge-sort.js"
 
-  test("first test", () => {
-    expect(mergeSort()).toBe("yes");
-  });
+
+test("Input is an empty array", () => {
+    expect(mergeSort()).toEqual([]);
+});
+
+test("Normal input, single unsorted array", () => {
+    expect(mergeSort([0, 1, 2, 4, 3, 7, 6])).toEqual([0, 1, 2, 3, 4, 6, 7]);
+});
+
+ 
